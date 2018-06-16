a72bd63db08b7bb17a15d36958817682e0845f65
FernandoPerezRivadeiro <39780355+FernandoPerezRivadeiro@users.noreply.github.com>
2018-06-16 13:13:14 -0700

    Add files via upload

---
+++ A2/CSIS3475-A2/Question6/Assignment2Question6/src/ListInterface.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public interface ListInterface {
+
+	public boolean isEmpty();
+	public int size();
+	
+	public void purhcase(int index, Object item)
+	throws ListIndexOutOfBoundsException, ListException;
+	
+	public void display 
+	
+}
